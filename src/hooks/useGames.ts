1591dd6ed1213daffc86da1e38665051621d510f
Brody Dentinger <brodydentinger@gmail.com>
2024-11-20 11:27:45 -0500

    custom hook to fetch games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+// From Rawg Games API Endpoint
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+  // From Rawg Game API Endpoint
+interface Game {
+    id: string;
+    name: string;
+}
+
+const useGames = () => {
+
+    
+
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+  
+    useEffect(() => {
+
+      const controller = new AbortController();
+
+      apiClient
+        .get<FetchGamesResponse>('/games', {signal: controller.signal})
+        .then((res) => setGames(res.data.results))
+        .catch((err) => {
+            if (err instanceof CanceledError) return;
+            setError(err.message)});
+
+      return () => controller.abort();
+    }, []);
+
+    return {games, error}
+}
+
+export default useGames;
