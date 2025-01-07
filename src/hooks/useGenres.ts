cb7be2dbfb7302348f567f9858fe1e138694d13d
Brody Dentinger <brodydentinger@gmail.com>
2025-01-07 13:59:47 -0500

    refactor platform hook to use react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,12 +2,12 @@ import {useQuery} from '@tanstack/react-query'
 import genres from "../data/genres";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
+
 export interface Genre {
     id: number;
     name: string;
     image_background: string;
 }
-
 
 const useGenres = () => useQuery({
     queryKey: ['genres'],
@@ -18,7 +18,5 @@ const useGenres = () => useQuery({
     staleTime: 24 * 60 * 60 * 1000, // 24hr
     initialData: { count: genres.length, results: genres}
 });
-
-  
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,16 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import useData, { FetchResponse } from "./useData";
 import { Platform } from "./useGames";
 
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => 
+        apiClient
+            .get<FetchResponse<Platform>>('/platforms/lists/parents')
+            .then(res => res.data),
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+    staleTime: 24 * 60 * 60 * 1000, // 24hr,
+        });
 
 export default usePlatforms;
